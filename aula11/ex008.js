f62eb41d35751bef7724b4981e2bfd02f3dd4189
Wesley Freire <wesleyfreire1707@gmail.com>
2022-10-22 13:38:50 -0300

    atualização

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2;
+console.log(`a velocidade do seu carro é ${vel}km/h`);
+if (vel > 60) {
+  console.log(`voce ultyrapassou a velocidade permitida. MULTADO`);
+}
+console.log(`DIRIJA SEMPRE USANDO CINTO DE SEGURANÇA`);
